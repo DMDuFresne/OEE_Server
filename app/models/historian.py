2b1607438ced1ea4e38211b9e12f53b1f1ec070a
DMDuFresne <Dylan@DuFresne.Social>
2023-06-10 13:34:36 -0500

    Changed Database to TimescaleDB for historical data

--- app/models/historian.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-class OEE:
-    def __init__(self, data):
-        self.availability = data['availability']
-        self.performance = data['performance']
-        self.quality = data['quality']
-        self.time = datetime.now()
